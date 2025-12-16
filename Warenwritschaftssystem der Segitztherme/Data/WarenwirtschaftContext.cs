58a32d6296f65d47cd26914c09f72745ea02c06f
Olena Savchenko <olena.savchenko.83@gmail.com>
2025-12-16 10:16:12 +0100

    Added foreigh keys

--- Warenwritschaftssystem der Segitztherme/Data/WarenwirtschaftContext.cs
+++ Warenwritschaftssystem der Segitztherme/Data/WarenwirtschaftContext.cs
@@ -14,5 +14,32 @@ namespace Warenwritschaftssystem_der_Segitztherme.Data
         public DbSet<HU> HUs { get; set; } = default!;
         public DbSet<Artikel> Artikels { get; set; } = default!;
         public DbSet<Lagerplatz> Lagerplaetze { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Foreign Key: Lagerplatz.LagerID -> Lager.LagerID
+            modelBuilder.Entity<Lagerplatz>()
+                .HasOne<Lager>()
+                .WithMany()
+                .HasForeignKey(lp => lp.LagerID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Foreign Key: HU.ArtikelID -> Artikel.ArtikelID
+            modelBuilder.Entity<HU>()
+                .HasOne<Artikel>()
+                .WithMany()
+                .HasForeignKey(hu => hu.ArtikelID)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Foreign Key: HU.LagerID -> Lager.LagerID
+            modelBuilder.Entity<HU>()
+                .HasOne<Lager>()
+                .WithMany()
+                .HasForeignKey(hu => hu.LagerID)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
+
