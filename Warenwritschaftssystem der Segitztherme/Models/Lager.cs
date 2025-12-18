91283e857bc30a940ad3dad2e592057d4632debf
Wai Jun Ao-Yong <waijun.ao-yong@swan.de>
2025-12-18 09:30:11 +0100

    Modified Home Index and add new file

--- Warenwritschaftssystem der Segitztherme/Models/Lager.cs
+++ Warenwritschaftssystem der Segitztherme/Models/Lager.cs
@@ -1,9 +1,12 @@
-﻿namespace Warenwritschaftssystem_der_Segitztherme.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Warenwritschaftssystem_der_Segitztherme.Models
 {
     public class Lager
     {
         public int LagerID { get; set; }
 
+        [Required(ErrorMessage = "Bitte eine Beschreibung eingeben.")]
         public string Beschreibung { get; set; }
 
         public Lager()
